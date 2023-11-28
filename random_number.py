d57cfdc3da7612e38f4605d575966fb30996b0aa
anusha <anu.rapuru@gmail.com>
2023-11-27 18:03:55 -0800

    ANUSHAS AMAZONG COMMIT

--- random_number.py
+++ random_number.py
@@ -2,3 +2,5 @@ import random
 
 # random.randint(a,b) generates a random number between a and b inclusive.
 print(f"The random number is {random.randint(0,9)}")
+print("AJKASNK this ANUSHAHAHAAHHAHA, ITS MY REPOOO!! MWAHAH")
+print("NAWWWRRRRR")
